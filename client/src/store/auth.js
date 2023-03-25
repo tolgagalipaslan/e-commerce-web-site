6b7101f9c2bb78d2c108ed9907e9031162d611d2
tolgagalipaslan <tolgagalipaslan1999@gmail.com>
2023-03-25 23:21:08 +0300

    bug Fix

--- client/src/store/auth.js
+++ client/src/store/auth.js
@@ -69,7 +69,7 @@ export const authSlice = createSlice({
         const newUser = {
           userName: action.payload.name,
           email: action.payload.email,
-          userId: action.payload.subId,
+          userId: userId,
         };
 
         localStorage.setItem("user", JSON.stringify(newUser));
